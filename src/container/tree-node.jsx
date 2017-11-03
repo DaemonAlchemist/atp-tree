1699610cb9c611226bcf2a48ded73abb85949d78
Andrea <andrea@wittrock.us>
2017-11-03 01:40:40 -0500

    Refactor tree node to use atp-dnd components

--- src/container/tree-node.jsx
+++ src/container/tree-node.jsx
@@ -4,53 +4,13 @@
 
 import {o} from "atp-sugar";
 import {connectWithLifecycle} from "react-lifecycle-component";
-import {DragSource, DropTarget} from 'react-dnd';
-
+import {dragSource, hierarchicalDropTarget} from 'atp-dnd';
+import {_} from 'atp-pointfree';
 import TreeNode from "../component/tree-node";
 import {openTreeNode, closeTreeNode, initTreeNode} from "../reducer/tree";
 
-const source = DragSource(
-    'tree-node',
-    {
-        beginDrag: (props, monitor, component) => ({id: props.id}),
-        endDrag: (props, monitor) => {
-            const result = monitor.getDropResult();
-            if(result) {
-                props.onMove(result);
-            }
-        }
-    },
-    (connect, monitor) => ({
-        dragSource: connect.dragSource(),
-        isDragging: monitor.isDragging()
-    })
-);
-
-const dropTarget = (action, name) => DropTarget(
-    'tree-node',
-    {
-        drop: (props, monitor) => ({
-            dropEffect: action,
-            sourceId: monitor.getItem().id,
-            targetId: props.id
-        }),
-        canDrop: (props, monitor) => {
-            const sourceId = monitor.getItem().id;
-            const restrictedNodes = (props.parents || []).concat(props.id);
-            const canDrop = !restrictedNodes.includes(sourceId);
-            return canDrop;
-        }
-    },
-    (connect, monitor) => ({
-        [name + 'DropTarget']: connect.dropTarget(),
-        [name + 'CanDrop']: monitor.canDrop(),
-        [name + 'IsOver']: monitor.isOver(),
-
-    })
-);
-
-const addChildTarget = dropTarget('into', 'addChild');
-const addAfterTarget = dropTarget('after', 'addAfter');
+const addChildTarget = hierarchicalDropTarget('tree-node', 'into', 'addChild');
+const addAfterTarget = hierarchicalDropTarget('tree-node', 'after', 'addAfter');
 
 //Generic tree node component
 const treeNodeContainer = connectWithLifecycle(
@@ -74,4 +34,4 @@ const treeNodeContainer = connectWithLifecycle(
     }))
 )(TreeNode);
 
-export default source(addChildTarget(addAfterTarget(treeNodeContainer)));
+export default _(dragSource('tree-node'), addChildTarget, addAfterTarget)(treeNodeContainer);
