0db6bf7010a74d99cf816020583dbda92c53a525
Andrea <andrea@wittrock.us>
2017-11-12 21:06:24 -0600

    Refactor all drop targets

--- src/component/tree-node.jsx
+++ src/component/tree-node.jsx
@@ -8,6 +8,16 @@ import {Draggable, DropTarget, Active, Inactive, CanDrop, CannotDrop} from 'atp-
 
 import TreeNode from "../container/tree-node";
 
+const DropTargetDiv = ({style}) =>
+    <div style={style || {}}>
+        <Active>
+            <CanDrop><div style={{height: "16px", border: "dashed 1px"}} /></CanDrop>
+            <CannotDrop><div style={{height: "16px", border: "dashed 1px red"}} /></CannotDrop>
+        </Active>
+        <Inactive><div style={{height: "4px", border: "none"}} /></Inactive>
+    </div>;
+
+
 const DropTargetPlaceholder = ({isOver, canDrop}) =>
     <div
         style={{
@@ -59,13 +69,7 @@ export default props =>
                         id={props.getId(props.obj)}
                         onReceiveDrop={props.onReceiveDrop}
                     >
-                        <div style={{marginLeft: "20px", width: "100%"}}>
-                            <Active>
-                                <CanDrop><div style={{height: "16px", border: "dashed 1px"}} /></CanDrop>
-                                <CannotDrop><div style={{height: "16px", border: "dashed 1px red"}} /></CannotDrop>
-                            </Active>
-                            <Inactive><div style={{height: "4px", border: "none"}} /></Inactive>
-                        </div>
+                        <DropTargetDiv style={{marginLeft: "20px", width: "100%"}}/>
                     </DropTarget>
                 </div>
             </Col>
@@ -107,13 +111,14 @@ export default props =>
         </ul>
         <Row>
             <Col xs={12}>
-                <div style={{textIndent: "-18px", marginLeft: "18px"}}>
-                    {props.addAfterDropTarget(
-                        <div style={{width: "100%"}}>
-                            <DropTargetPlaceholder isOver={props.addAfterIsOver} canDrop={props.addAfterCanDrop}/>
-                        </div>
-                    )}
-                </div>
+                <DropTarget
+                    action="after"
+                    accepts={props.accepts}
+                    id={props.getId(props.obj)}
+                    onReceiveDrop={props.onReceiveDrop}
+                >
+                    <DropTargetDiv style={{textIndent: "-18px", marginLeft: "18px", width: "calc(100% - 18px)"}}/>
+                </DropTarget>
             </Col>
         </Row>
     </li>;
